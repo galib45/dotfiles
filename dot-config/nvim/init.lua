04db3ca5b3618e952d4f609f9530c70746fb15cf
galib45 <asadulgalibvat69@gmail.com>
2023-12-26 06:20:21 +0600

    hudai

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -5,7 +5,6 @@ vim.g.netrw_altv = 1							-- change from left splitting to right splitting
 vim.g.netrw_liststyle=3						-- tree style view in netrw
 vim.opt.title = true							-- show title
 vim.cmd('set path+=**')						-- search current directory recursively
-vim.opt.syntax = "ON"
 vim.opt.backup = false
 vim.opt.compatible = false				-- turn off vi compatibility mode
 vim.opt.number = true							-- turn on line numbers
@@ -29,6 +28,8 @@ vim.opt.scrolloff = 6							-- scroll page when cursor is 8 lines from top/botto
 vim.opt.sidescrolloff = 6					-- scroll page when cursor is 8 spaces from left/right
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard and sync with unnamed register
 vim.opt.termguicolors = true			-- Enable true color support
+vim.opt.splitbelow = true					-- split go below
+vim.opt.splitright = true					-- vertical split to the right
 vim.cmd('colorscheme gruvbox')		-- set colorscheme
 vim.cmd('filetype plugin on')			-- set filetype 
 vim.cmd('set wildmenu')						-- enable wildmenu
@@ -43,16 +44,19 @@ function map(mode, lhs, rhs, opts)
 end
 
 -- reload config
-configdir = "$LOCALAPPDATA"
+configdir = "~/.config"
+if vim.fn.has("win64") or vim.fn.has("win32") then
+	configdir = "$LOCALAPPDATA"
+end
 map("n", "<leader>r", ":source "..configdir.."/nvim/init.lua<CR>")	
 
 -- normal mode keymaps
-map("n", "<leader>e", ":25Lex<CR>")	-- space+e toggles netrw tree view in 25% split
-map("n", "<C-s>", ":w<CR>")					-- ctrl+s saves the file
-map("n", "<Tab>", "i<C-t><Esc>")		-- indent one step
-map("n", "<S-Tab>", "i<C-d><Esc>")	-- remove indent one step
-map("n", "<leader>qq", ":q!<CR>")		-- quit nvim
-map("n", "<leader>bd", ":bd!<CR>") 			-- delete current buffer 
+map("n", "<leader>e", ":25Lex<CR>")				-- space+e toggles netrw tree view in 25% split
+map("n", "<C-s>", ":w<CR>")								-- ctrl+s saves the file
+map("n", "<Tab>", "i<C-t><Esc>")					-- indent one step
+map("n", "<S-Tab>", "i<C-d><Esc>")				-- remove indent one step
+map("n", "<leader>qq", ":q!<CR>")					-- quit nvim
+map("n", "<leader>bd", ":bd!<CR>") 				-- delete current buffer 
 
 -- insert mode keymaps
 map("i", "<C-s>", "<Esc>:w<CR>")				-- ctrl+s goes to normal mode and saves the file
