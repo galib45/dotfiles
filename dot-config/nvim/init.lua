02d602d38555906e99d791855df58390269110c3
Asadul Al Galib <asadulgalibvat69@gmail.com>
2024-06-29 17:32:58 +0600

    fix - rewrite neovim config in lua

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,87 +1,108 @@
-vim.g.mapleader = " "							-- sets leader key
-vim.g.netrw_banner = 0						-- gets rid of the annoying banner for netrw
-vim.g.netrw_browse_split = 4			-- open in prior window
-vim.g.netrw_altv = 1							-- change from left splitting to right splitting
-vim.g.netrw_liststyle=3						-- tree style view in netrw
-vim.opt.title = false							-- don't show title
-vim.cmd('set path+=**')						-- search current directory recursively
-vim.opt.backup = false
-vim.opt.compatible = false				-- turn off vi compatibility mode
-vim.opt.number = true							-- turn on line numbers
-vim.opt.relativenumber = true			-- turn on relative line numbers
-vim.opt.mouse = 'a'								-- enable the mouse in all modes
-vim.opt.ignorecase = true					-- enable case insensitive searching
-vim.opt.smartcase = true					-- all searches are case insensitive unless there's a capital letter
-vim.opt.hlsearch = false					-- disable all highlighted search results
-vim.opt.incsearch = true					-- enable incremental searching
-vim.opt.wrap = false
-vim.opt.tabstop = 2								-- tab = 2 spaces
-vim.opt.shiftwidth = 2						-- autoindent 2 spaces
-vim.opt.fileencoding = "utf-8"		-- encoding set to utf-8
-vim.opt.pumheight = 10						-- number of items in popup menu
-vim.opt.showtabline = 1						-- always show the tab line
-vim.opt.laststatus = 2						-- always show statusline
-vim.opt.signcolumn = "auto"
-vim.opt.expandtab = false
-vim.opt.smartindent = true
-vim.opt.scrolloff = 6							-- scroll page when cursor is 8 lines from top/bottom
-vim.opt.sidescrolloff = 6					-- scroll page when cursor is 8 spaces from left/right
-vim.opt.clipboard = "unnamedplus" -- Use system clipboard and sync with unnamed register
-vim.opt.termguicolors = true			-- Enable true color support
-vim.opt.splitbelow = true					-- split go below
-vim.opt.splitright = true					-- vertical split to the right
-vim.cmd('colorscheme catppuccin')		-- set colorscheme
-vim.cmd('filetype plugin on')			-- set filetype 
-vim.cmd('set wildmenu')						-- enable wildmenu
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
--- Functional wrapper for mapping custom keybindings
-function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+-- Make line numbers default
+vim.opt.number = true
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+-- Sync clipboard between OS and Neovim.
+vim.opt.clipboard = 'unnamedplus'
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.cmd('nohlsearch')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+local function get_plugin_name(repo)
+  -- Split the repository URL by '/'
+  local parts = {}
+  for part in repo:gmatch("[^/]+") do
+    table.insert(parts, part)
+  end
+  -- Extract the last part (plugin name)
+  local name = parts[#parts]
+  return name
 end
 
--- reload config
-configdir = "~/.config"
-map("n", "<leader>r", ":source "..configdir.."/nvim/init.lua<CR>")	
+-- Check if the operating system is Windows
+local is_windows = vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1
 
--- normal mode keymaps
-map("n", "<leader>e", ":25Lex<CR>")				-- space+e toggles netrw tree view in 25% split
-map("n", "<C-s>", ":w<CR>")								-- ctrl+s saves the file
-map("n", "<Tab>", "i<C-t><Esc>")					-- indent one step
-map("n", "<S-Tab>", "i<C-d><Esc>")				-- remove indent one step
-map("n", "<leader>qq", ":q!<CR>")					
--- buffer keymaps
-map("n", "<leader>bd", ":bd!<CR>") 				-- delete current buffer
-map("n", "<leader>bp", ":bp<CR>") 				-- go to the previous buffer
-map("n", "<leader>bn", ":bn<CR>") 				-- go to the next buffer
-map("n", "<leader>bl", ":ls<CR>") 				-- list all buffers
--- tab keymaps
-map("n", "<leader>tt", ":tabnext #<CR>")	-- go to the last accessed tab
-map("n", "<leader>tj", ":-tabnext<CR>")		-- go to the last accessed tab
-map("n", "<leader>tk", ":tabnext<CR>")		-- go to the last accessed tab
-map("n", "<leader>tn", ":tabnew<CR>")					-- create new tab
+local function install_plugin(repo)
+  local name = get_plugin_name(repo)
+  -- set the path separator acc to OS
+  local path_separator = '/'
+  if is_windows then path_separator = '\\' end
+  -- join plugin name with neovim data path
+  local path = vim.fn.stdpath('data') .. path_separator .. name
+  if not vim.loop.fs_stat(path) then
+    -- create complete repo url
+    repo = 'https://github.com/' .. repo
+    local success = vim.fn.system { 'git', 'clone', repo, path }
+    print(success)
+  end
+  -- add plugin directory to neovim runtime path
+  vim.opt.rtp:prepend(path)
+end
 
--- window keymaps
-map("n", "<leader>w", "<C-w>p")
-map("n", "<C-h>", "<C-w><C-h>")
-map("n", "<C-j>", "<C-w><C-j>")
-map("n", "<C-k>", "<C-w><C-k>")
-map("n", "<C-l>", "<C-w><C-l>")
+-- install plugins
+install_plugin('tpope/vim-sleuth')
+install_plugin('numToStr/Comment.nvim')
+install_plugin('echasnovski/mini.pairs')
+install_plugin('echasnovski/mini.statusline')
+install_plugin('folke/tokyonight.nvim')
+install_plugin('nvim-treesitter/nvim-treesitter')
+install_plugin('stevearc/oil.nvim')
 
--- insert mode keymaps
-map("i", "<C-s>", "<Esc>:w<CR>")				-- ctrl+s goes to normal mode and saves the file
+-- set the coloscheme
+vim.cmd('colorscheme tokyonight-night')
 
--- Automatically close brackets, parethesis, and quotes
-map("i", "'", "''<left>")
-map("i", "\"", "\"\"<left>")
-map("i", "(", "()<left>")
-map("i", "[", "[]<left>")
-map("i", "{", "{}<left>")
+-- setup autopairs
+require('mini.pairs').setup()
 
--- Visual mode keymaps
--- Terminal mode keymaps
-map("t", "<Esc>", "<C-\\><C-n>")
-map("t", "<leader>w", "<C-\\><C-n><C-w>p")
+-- setup the statusline
+local statusline = require 'mini.statusline'
+statusline.setup { use_icons = false }
+statusline.section_location = function()
+  return '%2l:%-2v'
+end
+
+-- setup treesitter
+require('nvim-treesitter.configs').setup({
+  auto_install = true,
+  highlight = { enable = true },
+})
+
+-- setup oil
+require("oil").setup()
+
+-- key mappings
+vim.keymap.set('n', '<C-s>', '<CMD>w<CR>')
+vim.keymap.set('i', '<C-s>', '<ESC><cmd>w<CR>')
+vim.keymap.set('n', '<C-r>', '<CMD>source %<CR>')
+vim.keymap.set('n', '<C-q>', '<CMD>q<CR>')
+vim.keymap.set("n", "-", "<CMD>Oil<CR>")
